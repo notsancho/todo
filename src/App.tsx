c0178658a4a03757af84427720b9db6ffdc08a91
Ilia Muzicica <ilia.muzicica@ebs-integrator.com>
2020-01-18 18:23:09 +0200

    drawer

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,43 @@ import localforage from "localforage";
 import ReactDOM from 'react-dom';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Row, Col } from 'antd';
+import { Row, Col, Drawer } from 'antd';
 import Aside from './components/Aside/Aside.module';
 import TodoList from './components/TodoList/TodoList.module';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
 
 const App: React.FC = props => {
-  
+  const [heightMenu, setHeightMenu] = useState(window.innerHeight - 30);
+
   return (
     <div className="wrap-todo">
       <Row>
-        <Col span={18} push={6}>
+
+        <BrowserView>
+          <Col span={18} push={6}>
+            <TodoList />
+          </Col>
+          <Col span={6} pull={18} className="wrap-aside">
+            <Aside />
+          </Col>
+        </BrowserView>
+        
+        <MobileView>
           <TodoList />
-        </Col>
-        <Col span={6} pull={18} className="wrap-aside">
-          <Aside />
-        </Col>
+          <Drawer
+            title="Basic Drawer"
+            placement="left"
+            visible={true}
+          >
+            <Aside />
+          </Drawer>
+        </MobileView>
+
       </Row>
     </div>
   );
